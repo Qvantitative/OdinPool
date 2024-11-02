cadedcc814b4729bc91762c4e161c96c8afc269a
Qvantitative <Qvantitative@gmail.com>
2024-11-02 02:06:35 -0400

    update next.config.js

--- app/next.config.js
+++ app/next.config.js
@@ -42,7 +42,27 @@ const nextConfig = {
       }
     ];
   },
-  // ... rest of your config
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*'
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS'
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, Content-Type, Accept'
+          }
+        ]
+      }
+    ];
+  }
 };
 
 module.exports = nextConfig;

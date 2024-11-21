5e416e55742f0fa214a71da2e5ee8470def7a7b6
Qvantitative <Qvantitative@gmail.com>
2024-11-20 23:49:21 -0500

    RunesTx

--- app/src/components/blocks/BlockDisplay.js
+++ app/src/components/blocks/BlockDisplay.js
@@ -15,25 +15,27 @@ const BlockDisplay = ({ block, onBlockClick }) => {
       aria-label={`Block #${block_height} mined by ${mining_pool || 'Unknown'} with estimated fee of ${fees_estimate || 'N/A'} sat/vB`}
       onClick={() => onBlockClick(block)}
     >
-      <h3 className="text-lg font-bold text-blue-500 md:text-xl">#{block_height}</h3>
-      <div className="relative perspective-3d m-2 md:m-4">
-        <div className="block-face bg-blue-500 text-white p-2 md:p-4 top-4 left-4 flex flex-col justify-between relative">
+      <h3 className="text-xl font-bold text-blue-500">#{block_height}</h3>
+      <div className="relative perspective-3d m-4">
+        <div className="block-face bg-blue-500 text-white p-4 top-4 left-4 flex flex-col justify-between relative">
           <div>
-            <p className="text-[10px] md:text-xs mb-1">{fees_estimate ? `${fees_estimate} sat/vB` : 'sat/vB'}</p>
-            <p className="text-[10px] md:text-xs mb-1">
+            <p className="text-xs mb-1">{fees_estimate ? `${fees_estimate} sat/vB` : 'sat/vB'}</p>
+            <p className="text-xs mb-1">
               {feeSpan && feeSpan.min !== null && feeSpan.max !== null && !isNaN(feeSpan.min) && !isNaN(feeSpan.max)
                 ? `${Number(feeSpan.min).toFixed(2)} - ${Number(feeSpan.max).toFixed(2)} sat/vB`
                 : 'Fee Span: Not Available'}
             </p>
-            <p className="text-[10px] md:text-xs">{inscriptions} Inscriptions</p>
-            <p className="text-[10px] md:text-xs">{transactions} Transactions</p>
+            <p className="text-xs">{inscriptions} Inscriptions</p>
+            <p className="text-xs">{transactions} Transactions</p>
           </div>
-          <p className="text-[10px] md:text-sm">{formattedTimestamp}</p>
+          <p className="text-sm">
+            {formattedTimestamp}
+          </p>
         </div>
-        <div className="block-side absolute h-full w-2 md:w-4 skew-y-[44deg] bg-[rgba(55,67,128,0.4)] dark:bg-[#233070] top-0 left-0 origin-top-left z-10"></div>
-        <div className="block-top absolute w-full h-2 md:h-4 skew-x-[45deg] bg-[rgba(55,67,128,0.2)] dark:bg-[#374590] top-0 right-0 origin-top-right z-10"></div>
+        <div className="block-side absolute h-full w-4 skew-y-[44deg] bg-[rgba(55,67,128,0.4)] dark:bg-[#233070] top-0 left-0 origin-top-left z-10"></div>
+        <div className="block-top absolute w-full h-4 skew-x-[45deg] bg-[rgba(55,67,128,0.2)] dark:bg-[#374590] top-0 right-0 origin-top-right z-10"></div>
       </div>
-      <h3 className="text-sm md:text-md text-yellow-500 p-1 md:p-2 ml-4 md:ml-6">{mining_pool || 'Unknown'}</h3>
+      <h3 className="text-md text-yellow-500 p-2 ml-6">{mining_pool || 'Unknown'}</h3>
     </div>
   );
 };

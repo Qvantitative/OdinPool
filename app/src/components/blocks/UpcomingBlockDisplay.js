789d02b99764b0274624a1cd383bf1709bb79acf
Qvantitative <Qvantitative@gmail.com>
2024-11-29 22:14:24 -0500

    runes Tx

--- app/src/components/blocks/UpcomingBlockDisplay.js
+++ app/src/components/blocks/UpcomingBlockDisplay.js
@@ -18,7 +18,10 @@ const UpcomingBlockDisplay = ({ block, mempoolSize }) => {
       <h3 className="text-xl font-bold text-orange-500">#{block_height}</h3>
       <div className="relative perspective-3d m-4">
         <div
-          className="block-face bg-orange-800 text-white p-4 top-4 left-4 flex flex-col justify-between relative animate-pulse-color"
+          className="block-face text-white p-4 top-4 left-4 flex flex-col justify-between relative animate-pulse-color"
+          style={{
+            background: 'radial-gradient(circle, #FFD700, #FF4500)', // Radiating yellow to orange gradient
+          }}
         >
           <div>
             <p className="text-xs mb-1">{fees_estimate ? `${fees_estimate} sat/vB` : 'sat/vB'}</p>
@@ -29,9 +32,7 @@ const UpcomingBlockDisplay = ({ block, mempoolSize }) => {
             </p>
             <p className="text-xs">{mempoolSize ? `${mempoolSize} Transactions in Mempool` : 'Mempool Size Not Available'}</p>
           </div>
-          <p className="text-sm">
-            ~ 10 min
-          </p>
+          <p className="text-sm">~ 10 min</p>
         </div>
         <div className="block-side absolute h-full w-4 skew-y-[44deg] bg-[rgba(255,165,0,0.4)] dark:bg-[#ffb84d] top-0 left-0 origin-top-left z-10 animate-pulse-color"></div>
         <div className="block-top absolute w-full h-4 skew-x-[45deg] bg-[rgba(255,165,0,0.2)] dark:bg-[#ffcc80] top-0 right-0 origin-top-right z-10 animate-pulse-color"></div>

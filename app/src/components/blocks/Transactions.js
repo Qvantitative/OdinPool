d37f98af1b2bfeabe6304601bac9a6633448c108
Qvantitative <Qvantitative@gmail.com>
2024-12-01 00:05:01 -0500

    runes Tx

--- app/src/components/blocks/Transactions.js
+++ app/src/components/blocks/Transactions.js
@@ -1,6 +1,7 @@
 // app/components/blocks/Transactions.js
 
 import React, { useState, useEffect } from 'react';
+import TransactionsTreeMap from './charts/TransactionsTreeMap';
 
 // Loading Circle Component
 const LoadingCircle = () => (
@@ -338,6 +339,10 @@ const Transactions = ({ transactionData, handleTransactionClick }) => {
       <h3 className="text-xl font-semibold mb-4 text-center text-white">
         Transactions ({transactionData.length} total)
       </h3>
+
+      {/* Add TreeMap component here */}
+      <TransactionsTreeMap transactionData={transactionData} />
+
       <div className="space-y-4">
         {currentTransactions.map(renderTransaction)}
       </div>

a0ec865dcde61887921f897f4cb2b63cf6a253df
Qvantitative <Qvantitative@gmail.com>
2024-12-03 23:50:15 -0500

    Transactions Map

--- app/src/components/blocks/charts/MempoolTreeMap.js
+++ app/src/components/blocks/charts/MempoolTreeMap.js
@@ -1,7 +1,5 @@
 // app/components/blocks/charts/MempoolTreeMap.js
 
-// app/components/blocks/charts/MempoolTreeMap.js
-
 import React, { useEffect, useRef, useMemo } from 'react';
 import * as d3 from 'd3';
 

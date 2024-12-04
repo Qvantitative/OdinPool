92f490b0a0e5d424f59a3f122d3041b00a6049e9
Qvantitative <Qvantitative@gmail.com>
2024-12-03 22:21:21 -0500

    Transactions Map

--- app/src/components/blocks/charts/MempoolTreeMap.js
+++ app/src/components/blocks/charts/MempoolTreeMap.js
@@ -12,9 +12,9 @@ const MempoolTreeMap = () => {
     const fetchTransactions = async () => {
       try {
         setIsLoading(true);
-        const response = await fetch('/api/mempool');
+        const response = await fetch('/api/transactions?block_height=null');
         if (!response.ok) {
-          throw new Error('Failed to fetch mempool transactions');
+          throw new Error('Failed to fetch transactions');
         }
         const data = await response.json();
         setTransactionData(data);
